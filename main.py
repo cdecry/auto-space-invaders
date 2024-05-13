2e630133b3d2fb248b75cd1e784e440e5eb15dee
crystal / ue <mfihy1@gmail.com>
2024-05-13 17:54:48 -0400

    Simple automation game loop

--- main.py
+++ main.py
@@ -1,62 +1,81 @@
-from Quartz import CGWindowListCopyWindowInfo, kCGNullWindowID, kCGWindowListOptionAll
+from Quartz.CoreGraphics import CGEventCreateMouseEvent, kCGEventMouseMoved, kCGEventLeftMouseDown, kCGEventLeftMouseUp, CGEventPost, kCGEventSourceStateHIDSystemState
+import Quartz
 import pyautogui
 from PIL import Image
 import time
-import os
+import keyboard
 from pathlib import Path
-
-def get_window_info(window_title):
-    windows = CGWindowListCopyWindowInfo(kCGWindowListOptionAll, kCGNullWindowID)
-    for window in windows:
-        if window_title in window.get("kCGWindowName", ""):
-            window_id = window["kCGWindowNumber"]
-            window_rect = window["kCGWindowBounds"]
-            x, y, w, h = window_rect["X"], window_rect["Y"], window_rect["Width"], window_rect["Height"]
-            return x, y, w, h
-    return None
-
-def capture_game_window(x, y, w, h):
-    game_window_pos = (x, y)
-    game_window_size = (w, h)
-
-    screenshot = pyautogui.screenshot(region=(int(game_window_pos[0]), int(game_window_pos[1]), int(game_window_size[0]), int(game_window_size[1])))
-    return screenshot
 
 def detect_image(img, confidence):
     pyautogui.useImageNotFoundException()
     detected = None
     try:
         detected = pyautogui.locateOnScreen(img, confidence=confidence)
-        print('image found')
     except pyautogui.ImageNotFoundException:
         pass
 
-    if detected:
-        return True
-    else:
-        return False
+    return detected
+
+def mouse_click(x, y):
+    move_event = CGEventCreateMouseEvent(None, kCGEventMouseMoved, (x, y), 0)
+    CGEventPost(kCGEventSourceStateHIDSystemState, move_event)
+    event = Quartz.CGEventCreateMouseEvent(None, kCGEventLeftMouseDown, (x, y), 0)
+    Quartz.CGEventPost(Quartz.kCGHIDEventTap, event)
+
+    time.sleep(0.1)
+
+    event = Quartz.CGEventCreateMouseEvent(None, kCGEventLeftMouseUp, (x, y), 0)
+    Quartz.CGEventPost(Quartz.kCGHIDEventTap, event)
+
 
 def main():
-    window_title = "Starimu"
-    window_info = get_window_info(window_title)
-    if not window_info:
-        print("Window not found")
-        return
-    
-    x, y, w, h = window_info
-    print(f"Window Found. Coordinates: x={x}, y={y}, width={w}, height={h}")
 
     while True:
-        # game_screenshot = capture_game_window(x, y, w, h)
-        if detect_image('exit_button.png', 0.7):
-            print("Game ended")
+        pyautogui.keyDown('space')
+        if keyboard.is_pressed(1):
             break
-
-        if detect_image('spaceship.png', 0.8):
-            print("Spaceship detected")
-
+            # pyautogui.keyUp('space')
+        spaceship_pos = detect_image('spaceship.png', 0.8)
         laser_pos = detect_image('laser.png', 0.97)
-            
+
+        if spaceship_pos:
+            # if laser_pos and laser_pos[0] >= spaceship_pos[0] and laser_pos[1] <= spaceship_pos[0] + spaceship_pos[2]:
+            #     pyautogui.keyDown('left')
+            #     time.sleep(0.2)
+            #     pyautogui.keyUp('left')
+
+            alien_pos = detect_image('alien1.png', 0.7)
+            if alien_pos:
+                if spaceship_pos[0] - alien_pos[0] > 10:
+                    pyautogui.keyDown('left')
+                    time.sleep(0.5)
+                    pyautogui.keyUp('left')
+                elif spaceship_pos[0] - alien_pos[0] < 10:
+                    pyautogui.keyDown('right')
+                    time.sleep(1)
+                    pyautogui.keyUp('right')
+
+        if detect_image('exit_button.png', 0.7):
+            mouse_click(715, 552)
+            time.sleep(4)
+            mouse_click(1183, 196)
+            time.sleep(0.5)
+
+            mouse_click(1002, 447)
+            time.sleep(0.5)
+
+            time.sleep(1)
+            mouse_click(718, 572)
+            time.sleep(1)
+            mouse_click(650, 511)
+            time.sleep(3)
+
+            mouse_click(718, 501)
+            time.sleep(1)
+            mouse_click(788, 433)
+            time.sleep(1)
+            mouse_click(715, 601)
+            time.sleep(3)
 
 if __name__ == "__main__":
     main()
